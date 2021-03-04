2ff567f33a90ed28d378994f5e8d9d8621d8e17b
Cristian cayun <krisshonero@gmail.com>
2021-03-04 01:13:43 -0300

    sharing functions by components

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos,myfunction1]=useState([
@@ -9,15 +11,32 @@ export default function App() {
     {text:'buy a coffe2', key:2},
     {text:'buy a coffe3', key:3}
   ]);
+
+  const pressHandler = (key)=>{
+    myfunction1((prevTodos)=>{
+      return prevTodos.filter(item=>item.key!=key);
+    })
+  } 
+
+  const sumbitHandler = (text) =>{
+    myfunction1((prevTodos)=>{
+      return [
+        {text:text, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header/>
-      <View style={styles.content}>        
+      <View style={styles.content}>   
+        <AddTodo submitHandler={sumbitHandler}/>     
         <View style={styles.list}>
           <FlatList
-            data={todos}
+            data={todos} 
             renderItem={({item})=>(
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler}/>
             )}
           />
         </View>
